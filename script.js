33d715126c13d77c96e6a238a3442dcca8626ae1
Davi Bernardes <daviroihero@gmail.com>
2023-12-09 09:59:29 -0300

    modificacoes e adicao de js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function toogleMode() {
-    alert('aoi')
+   const html = document.documentElement
+   html.classList.toggle("light")
+
+    const img = document.querySelector("#profile img")
+
+   if(html.classList.contains("light")) {
+
+    img.setAttribute("src","./assets/images/avatar-light.png")
+    img.setAttribute("alt","texto do modo ligth")
+} else{
+
+    img.setAttribute("src","./assets/images/avatar.png")
+    img.setAttribute("alt","texto quando o modo não é ligth")
+}
 }
